470f2dadceb1b965c0fd4c6af733815a4b7af0e0
Tera <24725862+teraa@users.noreply.github.com>
2022-11-06 22:12:14 +0100

    add design time dbcontext factory

---
+++ BattleTrace.Api/DesignTimeDbContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Teraa.Extensions.Configuration;
+using BattleTrace.Api.Options;
+using BattleTrace.Data;
+
+namespace BattleTrace.Api;
+
+[UsedImplicitly]
+internal class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .AddUserSecrets<Program>(optional: false)
+            .Build();
+
+        var dbOptions = config.GetOptions<DbOptions>();
+
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>()
+            .UseNpgsql(dbOptions.ConnectionString,
+                contextOptions =>
+                {
+                    contextOptions.MigrationsAssembly(typeof(Program).Assembly.FullName);
+                    contextOptions.CommandTimeout(600);
+                });
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
