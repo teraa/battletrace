9454c46c11741e0ec96e8323e5d1c5e451e2f973
Tera <24725862+teraa@users.noreply.github.com>
2022-11-07 00:31:08 +0100

    add deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# # edit sudoers file to allow for passwordless service stop/start
+# $ sudo visudo -f /etc/sudoers.d/battletrace
+# %sudo ALL=NOPASSWD: /bin/systemctl stop battletrace.service, /bin/systemctl start battletrace.service
+svc=battletrace.service
+
+echo -n "Status: "
+systemctl is-active $svc
+is_active=$?
+
+# exit on first error
+set -e
+
+if [[ $is_active -eq 0 ]]; then
+  echo "Stopping service"
+  sudo systemctl stop $svc
+fi
+
+echo "Moving files"
+rm -rf bin/current
+mv bin/{publish,current}
+
+if [[ $is_active -eq 0 ]]; then
+  echo "Starting service"
+  sudo systemctl start $svc
+fi
