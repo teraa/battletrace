1ad2da65bb58e13311d153746efd596ebbf28f16
Tera <24725862+teraa@users.noreply.github.com>
2022-11-06 22:07:02 +0100

    import from trace

--- BattleTrace.Api/Program.cs
+++ BattleTrace.Api/Program.cs
@@ -1,14 +1,64 @@
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Serilog;
+using Teraa.Extensions.AspNetCore;
+using Teraa.Extensions.Configuration;
+using BattleTrace.Api.Options;
+using BattleTrace.Data;
+
+Serilog.Debugging.SelfLog
+    .Enable(x => Console.WriteLine($"<4>SERILOG: {x}"));
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Host
+    .UseDefaultServiceProvider(options =>
+    {
+        options.ValidateOnBuild = true;
+        options.ValidateScopes = true;
+    })
+    .UseSystemd()
+    .UseSerilog((hostContext, options) =>
+    {
+        options.ReadFrom.Configuration(hostContext.Configuration);
+    });
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+builder.Services
+    .AddAsyncInitialization()
+    .AddControllers(options =>
+    {
+        options.ModelValidatorProviders.Clear();
+    })
+    .Services
+    .AddDbContext<AppDbContext>((services, options) =>
+    {
+        using var scope = services.CreateScope();
+        var dbOptions = scope.ServiceProvider
+            .GetRequiredService<IOptionsMonitor<DbOptions>>()
+            .CurrentValue;
+
+        options.UseNpgsql(dbOptions.ConnectionString, contextOptions =>
+        {
+            contextOptions.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
+        });
+
+#if DEBUG
+        options.EnableSensitiveDataLogging();
+#endif
+    })
+    .AddMediatR(typeof(Program))
+    .AddRequestValidationBehaviour()
+    .AddValidatorsFromAssemblyContaining<Program>()
+    .AddMemoryCache()
+    .AddHttpClient()
+    .AddHttpContextAccessor()
+    .AddOptionsWithValidation<DbOptions>()
+    ;
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseCors(policy =>
@@ -19,10 +69,10 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+await app.InitAsync();
+await app.RunAsync();
