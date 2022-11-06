077903be331acf7fe84d6a12a8b4fadff5d1a4c7
Tera <24725862+teraa@users.noreply.github.com>
2022-11-07 00:01:27 +0100

    initial model and migration

---
+++ BattleTrace.Data/Models/Server.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using BattleTrace.Data.Models;
+
+#pragma warning disable CS8618
+namespace BattleTrace.Data.Models
+{
+    [PublicAPI]
+    public class Server
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+    }
+}
+
+namespace BattleTrace.Data
+{
+    public partial class AppDbContext
+    {
+        public DbSet<Server> Servers { get; init; }
+    }
+}
