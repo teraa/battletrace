d1ee5c1e30bdc93d2991846cc33ee2bd6c0fc5ac
Tera <24725862+teraa@users.noreply.github.com>
2022-11-06 22:19:04 +0100

    new example controller and action

---
+++ BattleTrace.Api/Features/Servers/ServersController.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BattleTrace.Api.Features.Servers;
+
+[ApiController]
+[Route("[controller]")]
+public class ServersController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public ServersController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpGet]
+    public IActionResult Index(CancellationToken cancellationToken)
+        => Ok("Hi");
+}
