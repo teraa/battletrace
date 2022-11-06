84414db9cb27c887c0f5f86a495e5fa64ad4d11d
Tera <24725862+teraa@users.noreply.github.com>
2022-11-06 23:24:38 +0100

    move defaults to code

--- BattleTrace.Api/Options/DbOptions.cs
+++ BattleTrace.Api/Options/DbOptions.cs
@@ -7,7 +7,7 @@ namespace BattleTrace.Api.Options;
 #pragma warning disable CS8618
 public class DbOptions
 {
-    public string ConnectionString { get; init; }
+    public string ConnectionString { get; init; } = "Data Source=data.db";
 
     [UsedImplicitly]
     public class Validator : AbstractValidator<DbOptions>
