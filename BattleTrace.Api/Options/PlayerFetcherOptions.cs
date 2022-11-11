9a5061c365e9045f8a0e3096fa6079f37d09ca30
Tera <24725862+teraa@users.noreply.github.com>
2022-11-11 13:22:40 +0100

    lower default interval

--- BattleTrace.Api/Options/PlayerFetcherOptions.cs
+++ BattleTrace.Api/Options/PlayerFetcherOptions.cs
@@ -7,7 +7,7 @@ namespace BattleTrace.Api.Options;
 #pragma warning disable CS8618
 public class PlayerFetcherOptions
 {
-    public TimeSpan Interval { get; init; } = TimeSpan.FromMinutes(10);
+    public TimeSpan Interval { get; init; } = TimeSpan.FromMinutes(5);
     public TimeSpan BatchDelay { get; init; } = TimeSpan.FromSeconds(2);
     public int BatchSize { get; init; } = 60;
     public TimeSpan MaxServerAge { get; set; } = TimeSpan.FromDays(5);
