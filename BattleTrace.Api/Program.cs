affd3b808b31faf8e4d3bd8eb7e787ef8bb5d2c4
Tera <24725862+teraa@users.noreply.github.com>
2022-11-30 21:07:55 +0100

    Revert "remove redundant migration assembly configuration" - NOT redundant
    This reverts commit 12d84103d12f04fc24480f8736f8f68925ce782d.

--- BattleTrace.Api/Program.cs
+++ BattleTrace.Api/Program.cs
@@ -47,6 +47,7 @@ builder.Services
 
         options.UseSqlite(dbOptions.ConnectionString, contextOptions =>
         {
+            contextOptions.MigrationsAssembly(typeof(Program).Assembly.FullName);
             contextOptions.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
         });
 
