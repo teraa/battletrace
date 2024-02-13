de36d2f40b2c39df9625dbc7acf6b0425fc4089e
Tera <24725862+teraa@users.noreply.github.com>
2024-02-13 11:12:26 +0100

    update deploy.sh

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # # edit sudoers file to allow for passwordless service stop/start
-# $ sudo visudo -f /etc/sudoers.d/battletrace
-# %battletrace ALL=NOPASSWD: /bin/systemctl stop battletrace.service, /bin/systemctl start battletrace.service
+# $ sudo visudo -f /etc/sudoers.d/ci
+# %ci ALL=NOPASSWD: /bin/systemctl stop battletrace.service, /bin/systemctl start battletrace.service
 svc=battletrace.service
 
 echo -n "Status: "
