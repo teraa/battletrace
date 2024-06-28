1fe4733b4bb3552db3685f9115e3bb1453740c3c
Tera <24725862+teraa@users.noreply.github.com>
2024-06-28 22:48:30 +0200

    add DesignTimeDbContextFactory.cs

---
+++ BattleTrace/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using BattleTrace.Options;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Teraa.Extensions.Configuration;
+
+namespace BattleTrace.Data;
+
+[UsedImplicitly]
+internal sealed class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .AddUserSecrets<Program>(optional: false)
+            .Build();
+
+        var dbOptions = config.GetValidatedOptionsOrDefault<DbOptions>();
+
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>()
+            .UseSqlite(dbOptions.ConnectionString,
+                contextOptions =>
+                {
+                    contextOptions.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
+                });
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
