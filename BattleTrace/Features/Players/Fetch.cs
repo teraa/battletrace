66dd0915dd46760b9a5560aee0986491c3183f50
Tera <24725862+teraa@users.noreply.github.com>
2024-06-29 17:58:50 +0200

    refactor player fetch handler
    use Parallel.ForEachAsync

--- BattleTrace/Features/Players/Fetch.cs
+++ BattleTrace/Features/Players/Fetch.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
 using BattleTrace.Data;
 using BattleTrace.Data.Models;
 using JetBrains.Annotations;
@@ -42,77 +43,65 @@ public static class Fetch
             var minTimestamp = scanAt - _options.MaxServerAge;
 
             var servers = await _ctx.Servers
+                .Where(x => x.UpdatedAt > minTimestamp)
                 .ToListAsync(cancellationToken);
 
-            var serverIds = servers
-                .Where(x => x.UpdatedAt > minTimestamp)
-                .Select(x => x.Id)
-                .ToList();
+            _logger.LogDebug("Fetching players for {Servers} servers ", servers.Count);
 
-            _logger.LogDebug("Fetching players for {Servers} servers ", serverIds.Count);
+            var responses = new ConcurrentBag<(string serverId, DateTimeOffset updatedAt, Response response)>();
 
-            var players = new Dictionary<string, Data.Models.Player>();
-
-            var serverTasks = serverIds
-                .Select(x => new
-                {
-                    ServerId = x,
-                    Task = _client.GetServerSnapshot(x, cancellationToken)
-                })
-                .ToList();
-
-            await Task.WhenAll(serverTasks.Select(x => x.Task));
-            var now = DateTimeOffset.UtcNow;
-
-            foreach (var serverTask in serverTasks)
+            await Parallel.ForEachAsync(servers, cancellationToken, async (server, ct) =>
             {
-                using var httpResponse = await serverTask.Task;
+                using var httpResponse = await _client.GetServerSnapshot(server.Id, ct);
+                var updatedAt = DateTimeOffset.UtcNow;
 
                 if (!httpResponse.IsSuccessStatusCode)
                 {
                     _logger.LogDebug(
                         "Failed fetching players for {ServerId}, server returned: {StatusCode}: {ReasonPhrase}",
-                        serverTask.ServerId, (int) httpResponse.StatusCode, httpResponse.ReasonPhrase);
-                    continue;
+                        server.Id, (int) httpResponse.StatusCode, httpResponse.ReasonPhrase);
+
+                    return;
                 }
 
-                var server = await _ctx.Servers
-                    .Where(x => x.Id == serverTask.ServerId)
-                    .FirstAsync(cancellationToken);
+                var response = await httpResponse.Content.ReadFromJsonAsync<Response>(ct);
+                Debug.Assert(response is not null);
 
-                server.UpdatedAt = now;
+                server.UpdatedAt = updatedAt;
+                responses.Add((server.Id, updatedAt, response));
+            });
 
-                var response =
-                    await httpResponse.Content.ReadFromJsonAsync<Response>(cancellationToken: cancellationToken);
-
-                Debug.Assert(response is { });
-
-                var playersBatch = response.Snapshot.TeamInfo
+            var players = responses.SelectMany(x => x.response.Snapshot.TeamInfo
                     .SelectMany(t => t.Value.Players
-                        .Select(p => new Data.Models.Player
+                        .Select(p => new
                         {
-                            Id = p.Key,
-                            UpdatedAt = now,
-                            ServerId = serverTask.ServerId,
-                            Faction = t.Value.Faction,
-                            Team = int.Parse(t.Key),
-                            Name = p.Value.Name,
-                            Tag = p.Value.Tag,
-                            Rank = p.Value.Rank,
-                            Score = p.Value.Score,
-                            Kills = p.Value.Kills,
-                            Deaths = p.Value.Deaths,
-                            Squad = p.Value.Squad,
-                            Role = p.Value.Role,
-                        }));
-
-                foreach (var player in playersBatch)
-                    players[player.Id] = player;
-            }
+                            ServerId = x.serverId,
+                            UpdatedAt = x.updatedAt,
+                            Team = t,
+                            Player = p,
+                        })))
+                .GroupBy(x => x.Player.Key)
+                .Select(group => group.MaxBy(x => x.UpdatedAt)!)
+                .Select(x => new Data.Models.Player
+                {
+                    Id = x.Player.Key,
+                    UpdatedAt = x.UpdatedAt,
+                    ServerId = x.ServerId,
+                    Faction = x.Team.Value.Faction,
+                    Team = int.Parse(x.Team.Key),
+                    Name = x.Player.Value.Name,
+                    Tag = x.Player.Value.Tag,
+                    Rank = x.Player.Value.Rank,
+                    Score = x.Player.Value.Score,
+                    Kills = x.Player.Value.Kills,
+                    Deaths = x.Player.Value.Deaths,
+                    Squad = x.Player.Value.Squad,
+                    Role = x.Player.Value.Role,
+                })
+                .ToList();
 
             sw.Stop();
-            _logger.LogInformation("Fetched {Players} players from {Servers} servers in {Duration}",
-                players.Count, servers.Count, sw.Elapsed);
+            _logger.LogInformation("Fetched {Players} players from {Servers} servers in {Duration}", players.Count, servers.Count, sw.Elapsed);
 
             _ctx.PlayerScans.Add(new PlayerScan
             {
@@ -120,13 +109,14 @@ public static class Fetch
                 PlayerCount = players.Count,
             });
 
+            var playerIds = players.Select(x => x.Id).ToList();
             var playersToUpdate = await _ctx.Players
-                .Where(x => players.Keys.Contains(x.Id))
+                .Where(x => playerIds.Contains(x.Id))
                 .ToListAsync(cancellationToken);
 
             // Just delete and re-add all entries instead of bothering with change-tracking
             _ctx.Players.RemoveRange(playersToUpdate);
-            _ctx.Players.AddRange(players.Values);
+            _ctx.Players.AddRange(players);
             await _ctx.SaveChangesAsync(cancellationToken);
         }
 
