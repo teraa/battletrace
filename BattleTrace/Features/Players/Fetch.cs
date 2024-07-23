f57c3bc3d485b38524d4e807773c6eb0fd434000
Tera <24725862+teraa@users.noreply.github.com>
2024-07-23 19:29:27 +0200

    fix indent

--- BattleTrace/Features/Players/Fetch.cs
+++ BattleTrace/Features/Players/Fetch.cs
@@ -11,118 +11,118 @@ using Microsoft.Extensions.Options;
 
 namespace BattleTrace.Features.Players;
 
-    [UsedImplicitly]
-    public class Fetch
+[UsedImplicitly]
+public class Fetch
+{
+    private readonly PlayerFetcherOptions _options;
+    private readonly AppDbContext _ctx;
+    private readonly ILogger<Fetch> _logger;
+    private readonly IKeeperBattlelogApi _api;
+
+    public Fetch(
+        IOptionsMonitor<PlayerFetcherOptions> options,
+        AppDbContext ctx,
+        ILogger<Fetch> logger,
+        IKeeperBattlelogApi api)
     {
-        private readonly PlayerFetcherOptions _options;
-        private readonly AppDbContext _ctx;
-        private readonly ILogger<Fetch> _logger;
-        private readonly IKeeperBattlelogApi _api;
-
-        public Fetch(
-            IOptionsMonitor<PlayerFetcherOptions> options,
-            AppDbContext ctx,
-            ILogger<Fetch> logger,
-            IKeeperBattlelogApi api)
-        {
-            _ctx = ctx;
-            _logger = logger;
-            _api = api;
-            _options = options.CurrentValue;
-        }
-
-        public async Task Handle(CancellationToken cancellationToken)
-        {
-            var sw = Stopwatch.StartNew();
-
-            var scanAt = DateTimeOffset.UtcNow;
-            var minTimestamp = scanAt - _options.MaxServerAge;
-
-            var serverIds = await _ctx.Servers
-                .Where(x => x.UpdatedAt > minTimestamp)
-                .Select(x => x.Id)
-                .ToListAsync(cancellationToken);
-
-            _logger.LogDebug("Fetching players for {Servers} servers ", serverIds.Count);
-
-            var responses = new ConcurrentBag<(string serverId, DateTimeOffset updatedAt, IKeeperBattlelogApi.SnapshotResponse response)>();
-
-            await Parallel.ForEachAsync(serverIds, cancellationToken, async (serverId, ct) =>
-            {
-                var httpResponse = await _api.GetSnapshot(serverId, ct);
-                if (!httpResponse.IsSuccessStatusCode)
-                {
-                    _logger.LogDebug(
-                        "Failed fetching players for {ServerId}, server returned: {StatusCode} ({ReasonPhrase})",
-                        serverId, (int) httpResponse.StatusCode, httpResponse.ReasonPhrase);
-
-                    return;
-                }
-
-                responses.Add((serverId, DateTimeOffset.UtcNow, httpResponse.Content!));
-            });
-
-            var players = responses.SelectMany(x => x.response.Snapshot.TeamInfo
-                    .SelectMany(t => t.Value.Players
-                        .Select(p => new
-                        {
-                            ServerId = x.serverId,
-                            UpdatedAt = x.updatedAt,
-                            Team = t,
-                            Player = p,
-                        })))
-                .GroupBy(x => x.Player.Key)
-                .Select(group => group.MaxBy(x => x.UpdatedAt)!)
-                .Select(x => new Player
-                {
-                    Id = x.Player.Key,
-                    UpdatedAt = x.UpdatedAt,
-                    ServerId = x.ServerId,
-                    Faction = x.Team.Value.Faction,
-                    Team = int.Parse(x.Team.Key),
-                    Name = x.Player.Value.Name,
-                    NormalizedName = x.Player.Value.Name.ToLowerInvariant(),
-                    Tag = x.Player.Value.Tag,
-                    Rank = x.Player.Value.Rank,
-                    Score = x.Player.Value.Score,
-                    Kills = x.Player.Value.Kills,
-                    Deaths = x.Player.Value.Deaths,
-                    Squad = x.Player.Value.Squad,
-                    Role = x.Player.Value.Role,
-                })
-                .ToList();
-
-            sw.Stop();
-            _logger.LogInformation("Fetched {Players} players from {Servers} servers in {Duration}", players.Count,
-                serverIds.Count, sw.Elapsed);
-
-            await _ctx.Servers
-                .Where(x => serverIds.Contains(x.Id))
-                .ExecuteUpdateAsync(setters => setters
-                        .SetProperty(
-                            x => x.UpdatedAt,
-                            x => scanAt
-                        ),
-                    cancellationToken
-                );
-
-            var playerIds = players.Select(x => x.Id).ToList();
-
-            // Just delete and re-add all entries instead of bothering with change-tracking
-
-            await _ctx.Players
-                .Where(x => playerIds.Contains(x.Id))
-                .ExecuteDeleteAsync(cancellationToken);
-
-            await _ctx.BulkCopyAsync(players, cancellationToken);
-
-
-            _ctx.PlayerScans.Add(new PlayerScan
-            {
-                Timestamp = scanAt,
-                PlayerCount = players.Count,
-            });
-
-            await _ctx.SaveChangesAsync(cancellationToken);
-        }
+        _ctx = ctx;
+        _logger = logger;
+        _api = api;
+        _options = options.CurrentValue;
     }
+
+    public async Task Handle(CancellationToken cancellationToken)
+    {
+        var sw = Stopwatch.StartNew();
+
+        var scanAt = DateTimeOffset.UtcNow;
+        var minTimestamp = scanAt - _options.MaxServerAge;
+
+        var serverIds = await _ctx.Servers
+            .Where(x => x.UpdatedAt > minTimestamp)
+            .Select(x => x.Id)
+            .ToListAsync(cancellationToken);
+
+        _logger.LogDebug("Fetching players for {Servers} servers ", serverIds.Count);
+
+        var responses = new ConcurrentBag<(string serverId, DateTimeOffset updatedAt, IKeeperBattlelogApi.SnapshotResponse response)>();
+
+        await Parallel.ForEachAsync(serverIds, cancellationToken, async (serverId, ct) =>
+        {
+            var httpResponse = await _api.GetSnapshot(serverId, ct);
+            if (!httpResponse.IsSuccessStatusCode)
+            {
+                _logger.LogDebug(
+                    "Failed fetching players for {ServerId}, server returned: {StatusCode} ({ReasonPhrase})",
+                    serverId, (int) httpResponse.StatusCode, httpResponse.ReasonPhrase);
+
+                return;
+            }
+
+            responses.Add((serverId, DateTimeOffset.UtcNow, httpResponse.Content!));
+        });
+
+        var players = responses.SelectMany(x => x.response.Snapshot.TeamInfo
+                .SelectMany(t => t.Value.Players
+                    .Select(p => new
+                    {
+                        ServerId = x.serverId,
+                        UpdatedAt = x.updatedAt,
+                        Team = t,
+                        Player = p,
+                    })))
+            .GroupBy(x => x.Player.Key)
+            .Select(group => group.MaxBy(x => x.UpdatedAt)!)
+            .Select(x => new Player
+            {
+                Id = x.Player.Key,
+                UpdatedAt = x.UpdatedAt,
+                ServerId = x.ServerId,
+                Faction = x.Team.Value.Faction,
+                Team = int.Parse(x.Team.Key),
+                Name = x.Player.Value.Name,
+                NormalizedName = x.Player.Value.Name.ToLowerInvariant(),
+                Tag = x.Player.Value.Tag,
+                Rank = x.Player.Value.Rank,
+                Score = x.Player.Value.Score,
+                Kills = x.Player.Value.Kills,
+                Deaths = x.Player.Value.Deaths,
+                Squad = x.Player.Value.Squad,
+                Role = x.Player.Value.Role,
+            })
+            .ToList();
+
+        sw.Stop();
+        _logger.LogInformation("Fetched {Players} players from {Servers} servers in {Duration}", players.Count,
+            serverIds.Count, sw.Elapsed);
+
+        await _ctx.Servers
+            .Where(x => serverIds.Contains(x.Id))
+            .ExecuteUpdateAsync(setters => setters
+                    .SetProperty(
+                        x => x.UpdatedAt,
+                        x => scanAt
+                    ),
+                cancellationToken
+            );
+
+        var playerIds = players.Select(x => x.Id).ToList();
+
+        // Just delete and re-add all entries instead of bothering with change-tracking
+
+        await _ctx.Players
+            .Where(x => playerIds.Contains(x.Id))
+            .ExecuteDeleteAsync(cancellationToken);
+
+        await _ctx.BulkCopyAsync(players, cancellationToken);
+
+
+        _ctx.PlayerScans.Add(new PlayerScan
+        {
+            Timestamp = scanAt,
+            PlayerCount = players.Count,
+        });
+
+        await _ctx.SaveChangesAsync(cancellationToken);
+    }
+}
--- BattleTrace/Features/Servers/Fetch.cs
+++ BattleTrace/Features/Servers/Fetch.cs
@@ -7,86 +7,86 @@ using Microsoft.Extensions.Options;
 
 namespace BattleTrace.Features.Servers;
 
-    [UsedImplicitly]
-    public class Fetch
+[UsedImplicitly]
+public class Fetch
+{
+    private readonly ServerFetcherOptions _options;
+    private readonly AppDbContext _ctx;
+    private readonly ILogger<Fetch> _logger;
+    private readonly IBattlelogApi _api;
+
+    public Fetch(
+        IOptionsMonitor<ServerFetcherOptions> options,
+        AppDbContext ctx,
+        ILogger<Fetch> logger,
+        IBattlelogApi api)
     {
-        private readonly ServerFetcherOptions _options;
-        private readonly AppDbContext _ctx;
-        private readonly ILogger<Fetch> _logger;
-        private readonly IBattlelogApi _api;
-
-        public Fetch(
-            IOptionsMonitor<ServerFetcherOptions> options,
-            AppDbContext ctx,
-            ILogger<Fetch> logger,
-            IBattlelogApi api)
-        {
-            _ctx = ctx;
-            _logger = logger;
-            _api = api;
-            _options = options.CurrentValue;
-        }
-
-        public async Task Handle(CancellationToken cancellationToken)
-        {
-            var sw = Stopwatch.StartNew();
-
-            var servers = new Dictionary<string, IBattlelogApi.Server>();
-            int requestIndex = 0;
-            int lastSuccessfulIndex = 0;
-
-            do
-            {
-                int offset = requestIndex * _options.Offset;
-                var response = await _api.GetServers(offset, cancellationToken: cancellationToken);
-
-                int serversCount = servers.Count;
-                foreach (var server in response.Data)
-                {
-                    servers[server.Guid] = server;
-                }
-
-                if (serversCount != servers.Count)
-                    lastSuccessfulIndex = requestIndex;
-
-                _logger.LogDebug("Request {Request}: Found {NewServers} new servers, {TotalServers} total",
-                    requestIndex, servers.Count - serversCount, servers.Count);
-
-                requestIndex++;
-            } while (requestIndex < lastSuccessfulIndex + _options.Threshold);
-
-            sw.Stop();
-            _logger.LogInformation("Found {Servers} servers in {Requests} requests within {Duration}",
-                servers.Count, requestIndex, sw.Elapsed);
-
-            var now = DateTimeOffset.UtcNow;
-
-            var entities = servers.Values.Select(x => new Data.Models.Server
-            {
-                Id = x.Guid,
-                Name = x.Name,
-                IpAddress = x.Ip,
-                Port = x.Port,
-                Country = x.Country,
-                TickRate = x.TickRate,
-                UpdatedAt = now,
-            });
-
-            // Just delete and re-add all entries instead of bothering with change-tracking
-
-            await _ctx.Servers
-                .Where(x => servers.Keys.Contains(x.Id))
-                .ExecuteDeleteAsync(cancellationToken);
-
-            await _ctx.BulkCopyAsync(entities, cancellationToken);
-
-
-            _ctx.ServerScans.Add(new Data.Models.ServerScan
-            {
-                Timestamp = now,
-                ServerCount = servers.Count,
-            });
-
-            await _ctx.SaveChangesAsync(cancellationToken);
-        }
+        _ctx = ctx;
+        _logger = logger;
+        _api = api;
+        _options = options.CurrentValue;
     }
+
+    public async Task Handle(CancellationToken cancellationToken)
+    {
+        var sw = Stopwatch.StartNew();
+
+        var servers = new Dictionary<string, IBattlelogApi.Server>();
+        int requestIndex = 0;
+        int lastSuccessfulIndex = 0;
+
+        do
+        {
+            int offset = requestIndex * _options.Offset;
+            var response = await _api.GetServers(offset, cancellationToken: cancellationToken);
+
+            int serversCount = servers.Count;
+            foreach (var server in response.Data)
+            {
+                servers[server.Guid] = server;
+            }
+
+            if (serversCount != servers.Count)
+                lastSuccessfulIndex = requestIndex;
+
+            _logger.LogDebug("Request {Request}: Found {NewServers} new servers, {TotalServers} total",
+                requestIndex, servers.Count - serversCount, servers.Count);
+
+            requestIndex++;
+        } while (requestIndex < lastSuccessfulIndex + _options.Threshold);
+
+        sw.Stop();
+        _logger.LogInformation("Found {Servers} servers in {Requests} requests within {Duration}",
+            servers.Count, requestIndex, sw.Elapsed);
+
+        var now = DateTimeOffset.UtcNow;
+
+        var entities = servers.Values.Select(x => new Data.Models.Server
+        {
+            Id = x.Guid,
+            Name = x.Name,
+            IpAddress = x.Ip,
+            Port = x.Port,
+            Country = x.Country,
+            TickRate = x.TickRate,
+            UpdatedAt = now,
+        });
+
+        // Just delete and re-add all entries instead of bothering with change-tracking
+
+        await _ctx.Servers
+            .Where(x => servers.Keys.Contains(x.Id))
+            .ExecuteDeleteAsync(cancellationToken);
+
+        await _ctx.BulkCopyAsync(entities, cancellationToken);
+
+
+        _ctx.ServerScans.Add(new Data.Models.ServerScan
+        {
+            Timestamp = now,
+            ServerCount = servers.Count,
+        });
+
+        await _ctx.SaveChangesAsync(cancellationToken);
+    }
+}
