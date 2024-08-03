0eadb7b4525c513f6ae94401e806852f2b78c290
Tera <24725862+teraa@users.noreply.github.com>
2024-08-03 13:38:46 +0200

    add ValidationTests

---
+++ BattleTrace.Tests/ValidationTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using IndexPlayers = BattleTrace.Features.Players.Actions.Index;
+using IndexServers = BattleTrace.Features.Servers.Actions.Index;
+
+namespace BattleTrace.Tests;
+
+public class ValidationTests(AppFactory appFactory)
+    : AppFactoryTests(appFactory)
+{
+    [Fact]
+    public async Task IndexPlayers_ReturnsBadRequest_WhenInvalid()
+    {
+        using var scope = CreateScope();
+        var sender = scope.GetRequiredService<ISender>();
+        var request = new IndexPlayers.Query(null, null, null, Limit: 0);
+
+        var response = await sender.Send(request);
+
+        response.Should().BeOfType<BadRequestObjectResult>()
+            .Subject.Value.Should().BeOfType<ValidationProblemDetails>();
+    }
+
+    [Fact]
+    public async Task IndexServers_ReturnsBadRequest_WhenInvalid()
+    {
+        using var scope = CreateScope();
+        var sender = scope.GetRequiredService<ISender>();
+        var request = new IndexServers.Query(null, null, null, Limit: 0);
+
+        var response = await sender.Send(request);
+
+        response.Should().BeOfType<BadRequestObjectResult>()
+            .Subject.Value.Should().BeOfType<ValidationProblemDetails>();
+    }
+}
