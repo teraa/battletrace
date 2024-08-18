teraa/battletrace

commit e3258119ac05be95e5909ccfa12815fbb4e3eba3

use minimal apis

BattleTrace/Features/Players/Actions/Index.cs
@@ -11,12 +11,12 @@ namespace BattleTrace.Features.Players.Actions;
 public static class Index
 {
     public record Query(
-        [ModelBinder(Name = "id")] IReadOnlyList<string>? Ids,
+        [ModelBinder(Name = "id")] string?[] Ids,
         string? NamePattern,
         string? TagPattern,
         [ModelBinder(Name = "active")] bool ActiveOnly = false,
         int? Limit = null
-    ) : IRequest<IActionResult>;
+    ) : IRequest<IResult>;
 
     [UsedImplicitly]
     public class QueryValidator : AbstractValidator<Query>
@@ -45,7 +45,7 @@ public static class Index
         int Role);
 
     [UsedImplicitly]
-    public class Handler : IRequestHandler<Query, IActionResult>
+    public class Handler : IRequestHandler<Query, IResult>
     {
         private readonly AppDbContext _ctx;
 
@@ -54,11 +54,11 @@ public static class Index
             _ctx = ctx;
         }
 
-        public async Task<IActionResult> Handle(Query request, CancellationToken cancellationToken)
+        public async Task<IResult> Handle(Query request, CancellationToken cancellationToken)
         {
             var query = _ctx.Players.AsQueryable();
 
-            if (request.Ids is {Count: > 0})
+            if (request.Ids is {Length: > 0})
                 query = query.Where(x => request.Ids.Contains(x.Id));
 
             if (request.NamePattern is {Length: > 0})
@@ -83,7 +83,7 @@ public static class Index
                     .FirstOrDefaultAsync(cancellationToken);
 
                 if (lastScan == default)
-                    return new OkObjectResult(Array.Empty<Result>());
+                    return Results.Ok(Array.Empty<Result>());
 
                 query = query.Where(x => x.UpdatedAt >= lastScan);
             }
@@ -111,7 +111,7 @@ public static class Index
                     x.Role))
                 .ToListAsync(cancellationToken);
 
-            return new OkObjectResult(results);
+            return Results.Ok(results);
         }
     }
 }

BattleTrace/Features/Servers/Actions/Index.cs
@@ -15,7 +15,7 @@ public static class Index
         Guid? Id,
         [ModelBinder(Name = "ip")] string? IpAddress,
         int? Limit = null
-    ) : IRequest<IActionResult>;
+    ) : IRequest<IResult>;
 
     [UsedImplicitly]
     public class QueryValidator : AbstractValidator<Query>
@@ -36,7 +36,7 @@ public static class Index
         int Players);
 
     [UsedImplicitly]
-    public class Handler : IRequestHandler<Query, IActionResult>
+    public class Handler : IRequestHandler<Query, IResult>
     {
         private readonly AppDbContext _ctx;
 
@@ -45,7 +45,7 @@ public static class Index
             _ctx = ctx;
         }
 
-        public async Task<IActionResult> Handle(Query request, CancellationToken cancellationToken)
+        public async Task<IResult> Handle(Query request, CancellationToken cancellationToken)
         {
             var query = _ctx.Servers.AsQueryable();
 
@@ -91,7 +91,7 @@ public static class Index
                 .Select(x => new Result(x.Id, x.Name, x.IpAddress, x.Port, x.UpdatedAt, x.Players))
                 .ToListAsync(cancellationToken);
 
-            return new OkObjectResult(results);
+            return Results.Ok(results);
         }
     }
 }

BattleTrace.Tests/ValidationTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using IndexPlayers = BattleTrace.Features.Players.Actions.Index;
 using IndexServers = BattleTrace.Features.Servers.Actions.Index;
@@ -18,8 +19,8 @@ public class ValidationTests(AppFactory appFactory)
 
         var response = await sender.Send(request);
 
-        response.Should().BeOfType<BadRequestObjectResult>()
-            .Subject.Value.Should().BeOfType<ValidationProblemDetails>();
+        response.Should().BeOfType<BadRequest<ValidationProblemDetails>>()
+            .Subject.Value!.Errors.Keys.Should().BeEquivalentTo(["Limit"]);
     }
 
     [Fact]
@@ -31,7 +32,7 @@ public class ValidationTests(AppFactory appFactory)
 
         var response = await sender.Send(request);
 
-        response.Should().BeOfType<BadRequestObjectResult>()
-            .Subject.Value.Should().BeOfType<ValidationProblemDetails>();
+        response.Should().BeOfType<BadRequest<ValidationProblemDetails>>()
+            .Subject.Value!.Errors.Keys.Should().BeEquivalentTo(["Limit"]);
     }
 }

BattleTrace/Features/Players/PlayersController.cs
@@ -1,21 +1,0 @@
-﻿using MediatR;
-using Microsoft.AspNetCore.Mvc;
-using Index = BattleTrace.Features.Players.Actions.Index;
-
-namespace BattleTrace.Features.Players;
-
-[ApiController]
-[Route("[controller]")]
-public class PlayersController : ControllerBase
-{
-    private readonly ISender _sender;
-
-    public PlayersController(ISender sender)
-    {
-        _sender = sender;
-    }
-
-    [HttpGet]
-    public async Task<IActionResult> Index([FromQuery] Index.Query query, CancellationToken cancellationToken)
-        => await _sender.Send(query, cancellationToken);
-}

BattleTrace/Features/Servers/ServersController.cs
@@ -1,21 +1,0 @@
-﻿using MediatR;
-using Microsoft.AspNetCore.Mvc;
-using Index = BattleTrace.Features.Servers.Actions.Index;
-
-namespace BattleTrace.Features.Servers;
-
-[ApiController]
-[Route("[controller]")]
-public class ServersController : ControllerBase
-{
-    private readonly ISender _sender;
-
-    public ServersController(ISender sender)
-    {
-        _sender = sender;
-    }
-
-    [HttpGet]
-    public async Task<IActionResult> Index([FromQuery] Index.Query query, CancellationToken cancellationToken)
-        => await _sender.Send(query, cancellationToken);
-}

BattleTrace/Program.cs
@@ -5,9 +5,12 @@ using Teraa.Extensions.AspNetCore;
 using BattleTrace.Features.Players;
 using BattleTrace.Features.Servers;
 using BattleTrace.Data;
+using MediatR;
 using Teraa.Extensions.Configuration.Vault.Options;
 using Teraa.Extensions.Serilog.Systemd;
 using Teraa.Extensions.Serilog.Seq;
+using IndexPlayers = BattleTrace.Features.Servers.Actions.Index;
+using IndexServers = BattleTrace.Features.Servers.Actions.Index;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,7 +36,7 @@ builder.Services
     .Services
     .AddDb()
     .AddMediatR(config => { config.RegisterServicesFromAssemblyContaining<Program>(); })
-    .AddRequestValidationBehaviour()
+    .AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehaviour2<,>))
     .AddValidatorsFromAssemblyContaining<Program>()
     .AddMemoryCache()
     .AddHttpClient()
@@ -60,6 +63,18 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet(
+    "/players",
+    async ([AsParameters] IndexPlayers.Query query, ISender sender, CancellationToken cancellationToken)
+        => await sender.Send(query, cancellationToken)
+);
+
+app.MapGet(
+    "/servers",
+    async ([AsParameters] IndexServers.Query query, ISender sender, CancellationToken cancellationToken)
+        => await sender.Send(query, cancellationToken)
+);
 
 app.MapHangfire();
 

BattleTrace/RequestValidationBehaviour2.cs
@@ -1,0 +1,40 @@
+using FluentValidation;
+using JetBrains.Annotations;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BattleTrace;
+
+[PublicAPI]
+public class RequestValidationBehaviour2<TRequest, TResponse> : IPipelineBehavior<TRequest, IResult>
+    where TRequest : IRequest<TResponse>, IRequest<IResult>
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public RequestValidationBehaviour2(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+
+    public Task<IResult> Handle(TRequest request, RequestHandlerDelegate<IResult> next, CancellationToken cancellationToken)
+    {
+        if (!_validators.Any())
+            return next();
+
+        var context = new ValidationContext<TRequest>(request);
+
+        var errors = _validators
+            .Select(x => x.Validate(context))
+            .Where(x => !x.IsValid)
+            .SelectMany(x => x.Errors)
+            .GroupBy(x => x.PropertyName, x => x.ErrorMessage)
+            .ToDictionary(x => x.Key, x => x.ToArray());
+
+        if (!errors.Any())
+            return next();
+
+        var problemDetails = new ValidationProblemDetails(errors);
+
+        return Task.FromResult(Results.BadRequest(problemDetails));
+    }
+}

commit a65c342d5117ef792291b10e3c715b6b8697f856

fix model binding query param names in minimal api
this fixes a regression introduced in e3258119

BattleTrace/Features/Players/Actions/Index.cs
@@ -11,10 +11,10 @@ namespace BattleTrace.Features.Players.Actions;
 public static class Index
 {
     public record Query(
-        [ModelBinder(Name = "id")] string?[] Ids,
+        [FromQuery(Name = "id")] string?[] Ids,
         string? NamePattern,
         string? TagPattern,
-        [ModelBinder(Name = "active")] bool ActiveOnly = false,
+        [FromQuery(Name = "active")] bool ActiveOnly = false,
         int? Limit = null
     ) : IRequest<IResult>;
 

BattleTrace/Features/Servers/Actions/Index.cs
@@ -13,7 +13,7 @@ public static class Index
     public record Query(
         string? NamePattern,
         Guid? Id,
-        [ModelBinder(Name = "ip")] string? IpAddress,
+        [FromQuery(Name = "ip")] string? IpAddress,
         int? Limit = null
     ) : IRequest<IResult>;
 