0db597cbc4ed7aa26f4304d448b1423a8ccf0f27
Tera <24725862+teraa@users.noreply.github.com>
2024-08-20 21:35:51 +0200

    add HelpersTests

---
+++ BattleTrace.Tests/HelpersTests.cs
@@ -0,0 +1,28 @@
+﻿using BattleTrace.Common;
+using FluentAssertions;
+
+namespace BattleTrace.Tests;
+
+public class HelpersTests
+{
+    [Theory]
+
+    [InlineData("", "")]
+    [InlineData("foo", "foo")]
+
+    [InlineData("_", @"\_")]
+    [InlineData("%", @"\%")]
+    [InlineData("*", "%")]
+    [InlineData("?", "_")]
+
+    [InlineData("a_a", @"a\_a")]
+    [InlineData("a_", @"a\_")]
+    [InlineData("_a", @"\_a")]
+
+    public void StringToLikePatternTest(string input, string expectedResult)
+    {
+        var result = Helpers.StringToLikePattern(input);
+
+        result.Should().Be(expectedResult);
+    }
+}
